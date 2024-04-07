0ef1a9bfeac664dea2822972d1c14606fdc4f6d2
jay-lab <forbia3210123@gmail.com>
2024-04-07 23:39:19 +0900

    terraform init

---
+++ src/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2" # Asia Pacific (Seoul) region
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
