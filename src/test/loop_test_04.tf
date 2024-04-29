d96ec33e3942e65716b16a136add6c0bb0623399
jay-lab <forbia3210123@gmail.com>
2024-04-29 15:03:00 +0900

    map에 대한 for 표현식

--- src/test/loop_test_04.tf
+++ src/test/loop_test_04.tf
@@ -1,3 +1,4 @@
+# 단일 값에 대한 for 표현식
 variable "user_names05" {
   description = "Create IAM users with these names"
   type        = list(string)
@@ -21,3 +22,36 @@ output "short_upper_names" {
 #short_upper_names = [
 # "NEO",
 #]
+
+#######################################################################
+# map 값에 대한 for 표현식
+variable "hero_thousand_faces" {
+  description = "map"
+  type        = map(string)
+  default = {
+    neo      = "hero"
+    trinity  = "love interest"
+    morpheus = "mentor"
+  }
+}
+# map의 key, value를 가지고 가공된 list 값 구성하기
+output "bios" {
+  value = [for name, role in var.hero_thousand_faces : "${name} is the ${role}"]
+}
+# ✨✨✨✨✨ plan 결과 ✨✨✨✨✨
+#bios = [
+#  "morpheus is the mentor",
+#  "neo is the hero",
+#  "trinity is the love interest",
+#]
+
+# map의 key, value 가공하기
+output "upper_roles" {
+  value = {for name, role in var.hero_thousand_faces : upper(name) => upper(role)}
+}
+# ✨✨✨✨✨ plan 결과 ✨✨✨✨✨
+#upper_roles = {
+#  MORPHEUS = "MENTOR"
+#  NEO      = "HERO"
+#  TRINITY  = "LOVE INTEREST"
+#}
